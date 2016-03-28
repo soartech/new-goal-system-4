6be7b9afcf472a8d102c8f822e750c8dea8484f5
Jacob Crossman <jcrossman@soartech.com>
2016-03-28 17:54:05 -0400

    Added a 2d distance macro.

--- _core/_CORE_Math.tcl
+++ _core/_CORE_Math.tcl
@@ -38,3 +38,7 @@ proc MATH_Rad_to_Deg { radians } {
 	variable CORE_MATH_PI
 	return [expr $radians * (180 / $CORE_MATH_PI)] 
 }
+
+proc MATH_Distance2d { x1 y1 x2 y2 } {
+    return "(sqrt (+ (* (- $x1 $x2) (- $x1 $x2)) (* (- $y1 $y2) (- $y1 $y2)) ))"
+}
