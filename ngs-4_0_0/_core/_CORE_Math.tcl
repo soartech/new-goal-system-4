3fae9e783faed28064b52f5dc64e5a562fc355de
Bob Marinier <bob.marinier@soartech.com>
2017-01-02 10:31:48 -0500

    Added more math functions, primarily to support finding distances between lat/long locations

--- ngs-4_0_0/_core/_CORE_Math.tcl
+++ ngs-4_0_0/_core/_CORE_Math.tcl
@@ -29,14 +29,16 @@
 #
 
 CORE_CreateMacroVar CORE_MATH_PI 3.14159265359
+CORE_CreateMacroVar CORE_MATH_RAD_TO_DEG [expr 180.0 / $CORE_MATH_PI]
+CORE_CreateMacroVar CORE_MATH_DEG_TO_RAD [expr $CORE_MATH_PI / 180.0]
 
 proc MATH_Deg_to_Rad { degrees } { 
-	variable CORE_MATH_PI
-	return [expr $degrees * ($CORE_MATH_PI / 180.0)] 
+    variable CORE_MATH_PI
+    return [expr $degrees * ($CORE_MATH_PI / 180.0)] 
 }
 proc MATH_Rad_to_Deg { radians } { 
-	variable CORE_MATH_PI
-	return [expr $radians * (180 / $CORE_MATH_PI)] 
+    variable CORE_MATH_PI
+    return [expr $radians * (180 / $CORE_MATH_PI)] 
 }
 
 proc MATH_Distance2d { x1 y1 x2 y2 } {
@@ -56,4 +58,60 @@ proc MATH_Distance2d { x1 y1 x2 y2 } {
 proc MATH_DistanceToLine2d { p_x p_y p1_x p1_y p2_x p2_y } {
     return "(/ (abs (+ (* (- $p2_y $p1_y) $p_x) (* (- $p1_x $p2_x) $p_y) (* $p2_x $p1_y) (* -1.0 (* $p2_y $p1_x)) )) [MATH_Distance2d $p1_x $p1_y $p2_x $p2_y])"
 
+}
+
+# f: a floating point number (constant or variable)
+# returns an expression that rounds to the nearest integer
+proc MATH_Round { f } {
+    return "(int (+ 0.5 $f))"
+}
+
+## MATH_Sin_Deg:
+## return the sine of a value/rhs-func given in degrees
+proc MATH_Sin_Deg { a } {
+   variable CORE_MATH_DEG_TO_RAD
+   return "(sin (* $a $CORE_MATH_DEG_TO_RAD))"
+}
+
+## MATH_Cos_Deg:
+## return the cosine of a value/rhs-func given in degrees
+proc MATH_Cos_Deg { a } {
+   variable CORE_MATH_DEG_TO_RAD
+   return "(cos (* $a $CORE_MATH_DEG_TO_RAD))"
+}
+
+## MATH_Distance3dSquared:
+## return the squared 3d distance between points (x0,y0,z0) and (x1,y1,z1),
+## or the squared length of the vector (x0,y0,z0).
+proc MATH_Distance3dSquared { x0 y0 z0 {x1 0} {y1 0} {z1 0} } {
+   return "(+ (* (- $x0 $x1) (- $x0 $x1)) (* (- $y0 $y1) (- $y0 $y1)) (* (- $z0 $z1) (- $z0 $z1)))"
+}
+
+## MATH_Distance3d:
+## return the 3d distance between points (x0,y0,z0) and (x1,y1,z1),
+## or the length of the vector (x0,y0,z0).
+proc MATH_Distance3d { x0 y0 z0 {x1 0} {y1 0} {z1 0} } {
+   return "(sqrt [MATH_Distance3dSquared $x0 $y0 $z0 $x1 $y1 $z1])"
+}
+
+## MATH_DistanceEarth:
+## Return the distance between two points on the idealized, spherical, Earth
+## Defined by their latitudes and longitudes (in degrees)
+proc MATH_DistanceEarth { lat1 long1 lat2 long2 } {
+    return "[MATH_DistanceSpherical $lat1 $long1 $lat2 $long2 6378000.0]"
+}
+
+
+## MATH_DistanceSpherical:
+## return the distance between two points on a sphere of radius R
+## defined by their latitudes and longitudes (in degrees)
+proc MATH_DistanceSpherical { lat1 long1 lat2 long2 radius } {
+    return "(* $radius [MATH_Distance3d \
+              [MATH_Sin_Deg $lat1] \
+              "(* [MATH_Cos_Deg $lat1] [MATH_Cos_Deg $long1])" \
+              "(* [MATH_Cos_Deg $lat1] [MATH_Sin_Deg $long1])" \
+              [MATH_Sin_Deg $lat2] \
+              "(* [MATH_Cos_Deg $lat2] [MATH_Cos_Deg $long2])" \
+              "(* [MATH_Cos_Deg $lat2] [MATH_Sin_Deg $long2])" \
+            ])"
 }
