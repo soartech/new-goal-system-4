f0fa2f62f59084c0fa371aeddf8c69f235de28e3
JACOB CROSSMAN <jacobcrossman@JACOBs-MacBook-Pro.local>
2017-03-17 17:50:38 -0400

    Added support macros for internal operating picture explanation.

--- ngs-4_0_0/behavior-explanation/types.tcl
+++ ngs-4_0_0/behavior-explanation/types.tcl
@@ -48,3 +48,88 @@ NGS_DeclareType NGS_Explain_TaskAwareness {}
 
 # TODO: Fill in
 NGS_DeclareType NGS_Explain_InternalOperatingPicture {}
+
+
+
+
+###################### Spatial Types ############################
+# These are the built in IOP spatial object types. Feel free to derive
+#  from them to create your own.
+
+# Root spatial object (all spatial objects should inherit from this)
+NGS_DeclareType IOPSpatialObject {
+    name     ""
+    children ""
+    location ""
+}
+
+NGS_DeclareType IOPPoint {
+    type IOPSpatialObject
+}
+
+NGS_DeclareType IOPDestination {
+    type { IOPSpatialObject IOPPoint }
+}
+
+NGS_DeclareType IOPWaypoint {
+    type { IOPSpatialObject IOPPoint }
+}
+
+NGS_DeclareType IOPArea {
+    type IOPSpatialObject
+}
+
+NGS_DeclareType IOPCircle {
+    type { IOPSpatialObject IOPArea }
+    radius ""
+}
+
+NGS_DeclareType IOPPolygon {
+    type { IOPSpatialObject IOPArea }
+    points ""
+}
+
+NGS_DeclareType IOPPolyLine {
+    type IOPSpatialObject
+    points ""
+}
+
+NGS_DeclareType IOPRoute {
+    type { IOPSpatialObject IOPPolyLine }
+    route-id ""
+}
+
+NGS_DeclareType IOPPhaseLine {
+    type { IOPSpatialObject IOPPolyLine }
+}
+
+NGS_DeclareType IOPOrientedObject {
+    type IOPSpatialObject
+    orientation ""
+}
+
+NGS_DeclareType IOPMovingObject {
+    type { IOPSpatialObject IOPOrientedObject }
+    velocity ""
+}
+
+NGS_DeclareType IOPEntity {
+    type { IOPSpatialObject IOPOrientedObject IOPMovingObject }
+}
+
+NGS_DeclareType IOPHuman {
+    type { IOPSpatialObject IOPOrientedObject IOPMovingObject IOPEntity }
+}
+
+NGS_DeclareType IOPVehicle {
+    type { IOPSpatialObject IOPOrientedObject IOPMovingObject IOPEntity }
+}
+
+NGS_DeclareType IOPUGV {
+    type { IOPSpatialObject IOPOrientedObject IOPMovingObject IOPEntity IOPVehicle }
+}
+
+NGS_DeclareType IOPUAV {
+    type { IOPSpatialObject IOPOrientedObject IOPMovingObject IOPEntity IOPVehicle }
+}
+
